2069c83064baf53a33f8d8bec982aab09c9d5504
Mariano Quipildor <marianogq@gmail.com>
2020-10-21 22:23:11 -0300

    docs agregado

---
+++ docs/precache-manifest.ce3aacc41c9aa6572ab4d4d253ff9614.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8b91dc3b52c74e6641cced53fd0854b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2136e89a632e0568ce4b",
+    "url": "/static/js/2.6d1893b9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6d1893b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0143aa65221b199283df",
+    "url": "/static/js/main.fb599ba1.chunk.js"
+  },
+  {
+    "revision": "94d7ca25d4df19621824",
+    "url": "/static/js/runtime-main.b7f813af.js"
+  }
+]);
---
+++ src/routers/AppRouter.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+  } from "react-router-dom";
+import { LoginScreen } from '../components/login/LoginScreen';
+import { DashboardRouters } from './DashboardRouters';
+
+export const AppRouter = () => {
+    return (
+        <Router>
+            <div>
+                <Switch>
+                    <Route exact path="/login" component={LoginScreen} />
+                    <Route path="/" component={DashboardRouters} />
+                </Switch>
+            </div>
+        </Router>
+    )
+}
